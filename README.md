63a3aad5ffb11235618daf8bbfc3b8cc1c996300
Shoan Chikato <sjchikato@gmail.com>
2017-07-07 01:37:59 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Passport Login 
+
+Setting up local authentication and authoration using Passport 
+
+## Installation
+
+Prerequisite 
+
+mondoDB running on default port
+
+```bash
+$ npm install  
+$ npm start  
+```
+
+## Features
+
+* Managing Users
+* Local Authentication
+* Local Authorisation
+
+## License
+
+Licensed under the MIT License
---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+const mongoose = require('mongoose');
+
+const port = 3000;
+
+// Routes Files
+const index = require('./routes/index');
+
+
+// inti app
+const app = express();
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Express Sessions
+app.use(session({
+	secret: 'secret',
+	saveUninitialized: true,
+	resave: true
+}));
+
+// Init passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Messages
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: (param, msg, value) => {
+      let namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+ 
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+app.use('/', index);
+
+
+// Start Server
+app.listen(port, () => {
+	console.log(`Server started on port ${port}`);
+});
